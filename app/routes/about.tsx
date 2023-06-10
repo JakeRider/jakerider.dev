614a6656a868af9dbaa7ded055da836f5eb3d568
Jake Rider <jake.rider@hingehealth.com>
2023-06-09 23:08:44 -0700

    Add a second page

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -3,16 +3,16 @@ import { Link } from '@remix-run/react';
 
 export const meta: V2_MetaFunction = () => {
   return [
-    { title: 'New Remix App' },
+    { title: 'Jake Rider | About' },
     { name: 'description', content: 'Welcome to Remix!' },
   ];
 };
 
-export default function Index() {
+export default function About() {
   return (
     <div className="mx-auto mt-16 max-w-7xl text-center">
-      <Link to="/about" className="text-xl text-blue-600 underline">
-        About
+      <Link to="/" className="text-xl text-blue-600 underline">
+        Home
       </Link>
     </div>
   );
