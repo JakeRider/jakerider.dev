e43106c483a8e8660103f5efce5ac7247e362864
Jake Rider <jake.rider@hingehealth.com>
2023-06-12 12:38:19 -0400

    Tidy remix config

--- remix.config.js
+++ remix.config.js
@@ -1,10 +1,5 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  ignoredRouteFiles: ['**/.*'],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
   future: {
     v2_errorBoundary: true,
     v2_headers: true,
@@ -12,6 +7,7 @@ module.exports = {
     v2_normalizeFormMethod: true,
     v2_routeConvention: true,
   },
+  ignoredRouteFiles: ['**/.*'],
   postcss: true,
   serverModuleFormat: 'cjs',
   tailwind: true,
